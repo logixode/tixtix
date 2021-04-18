475510243383ebff530b2d7237c58f47b9ea08ce
logixode <rohmad1230@gmail.com>
2021-04-18 09:57:45 +0700

    Error Hanling & Spinner Loading when login added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:tixtix/bloc/blocs.dart';
+import 'package:tixtix/bloc/user_bloc.dart';
 import 'package:tixtix/services/services.dart';
 import 'package:tixtix/ui/pages/pages.dart';
 
@@ -15,10 +16,17 @@ class MyApp extends StatelessWidget {
     return StreamProvider.value(
       value: AuthServices.userStrema,
       child: MultiBlocProvider(
-        providers: [BlocProvider(create: (_) => PageBloc())],
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: Wrapper(),
+        providers: [
+          BlocProvider(create: (_) => PageBloc()),
+          BlocProvider(create: (_) => UserBloc()),
+          BlocProvider(create: (_) => ThemeBloc())
+        ],
+        child: BlocBuilder<ThemeBloc, ThemeState>(
+          builder: (_, themeState) => MaterialApp(
+            theme: themeState.themeData,
+            debugShowCheckedModeBanner: false,
+            home: Wrapper(),
+          ),
         ),
       ),
     );
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,8 +1,12 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tixtix/bloc/blocs.dart';
+import 'package:tixtix/bloc/theme_bloc.dart';
 import 'package:tixtix/services/services.dart';
 import 'package:tixtix/shared/shared.dart';
 
